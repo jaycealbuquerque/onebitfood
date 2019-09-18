486b49cb790926aee13db1c1ffd03e5ea85dbd72
Jeyce Albuquerque <jeyce@iMac-de-Jeyce.local>
2019-09-18 19:20:04 -0300

    formataçao do JSON devolvido

--- app/serializers/category_serializer.rb
+++ app/serializers/category_serializer.rb
@@ -1,3 +1,10 @@
 class CategorySerializer < ActiveModel::Serializer
-  attributes :id
+  include Rails.application.routes.url_helpers
+	
+  # campos que o objeto devolve quando os campos forem renderizado
+	attributes :id, :title, :image_url
+	   
+	def image_url
+	  rails_blob_url(object.image)
+	end
 end
--- app/serializers/restaurant_serializer.rb
+++ app/serializers/restaurant_serializer.rb
@@ -1,3 +1,24 @@
-class RestaurantSerializer < ActiveModel::Serializer
-  attributes :id
+ class RestaurantSerializer < ActiveModel::Serializer
+ 
+	  include Rails.application.routes.url_helpers
+
+    # campos que o objeto devolve quando os campos forem renderizado
+	  attributes :id, :name, :description, :review, :status, :delivery_tax, :state,
+	    :city, :street, :neighborhood, :number, :complement,
+	    :reference, :cep, :image_url, :category_title
+
+	  has_many :product_categories, if: -> { @instance_options[:product_categories]}
+
+	  def image_url
+	    rails_blob_url(object.image)
+	  end
+
+	  def review
+	    object.reviews&.average(:value)
+	  end
+
+	  def category_title
+	    "cozinha #{object.category&.title}"
+	  end
+	
 end
